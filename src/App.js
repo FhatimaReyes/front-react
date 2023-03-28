b28bd9c666e27b9433d8ba8c489358314a44fc1f
FhatimaReyes <fhareal10@gmail.com>
2023-03-28 10:10:52 -0600

    update front-react

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Fhamono mono mono 
+         reload fha 🐳
         </p>
         <a
           className="App-link"
